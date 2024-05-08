947e44b38946b41180aa2ae71dacb2962c2397a3
Pablo Estigarribia <4000Pablo@gmail.com>
2024-05-08 01:04:52 -0300

    part1 actualizada hasta el ejercicio 1.3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,20 +1,26 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React: ",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data: ",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component: ",
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header course={course} />
       <Content
-        parts={[part1, part2, part3]}
-        exercises={[exercises1, exercises2, exercises3]}
+        parts={[part1.name, part2.name, part3.name]}
+        exercises={[part1.exercises, part2.exercises, part3.exercises]}
       />
-      <Total sum={exercises1 + exercises2 + exercises3} />
+      <Total sum={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
@@ -42,7 +48,7 @@ const Part = (props) => {
 };
 
 const Total = (props) => {
-  return <p>Number of exercises {props.sum}</p>;
+  return <p>Number of exercises: {props.sum}</p>;
 };
 
 export default App;
